a470c643b5770210d7eb3e7d575ce82fc977e180
Pavel Zakharov <zotic1001@gmail.com>
2024-06-26 13:43:16 +0300

    Добавлена возможность получения значения ВВП без сезонного фактора в ценах 2021г

--- parser/gdp.py
+++ parser/gdp.py
@@ -31,6 +31,47 @@ def to_date(year, quarter):
     return datetime(year, quarter_to_month[quarter], 1)
 
 
+def get_gdp_dataframe_wout_seasons():
+    download_gdp()
+    df = pd.read_excel(os.path.join(os.path.join("files", "gdp"), f"VVP_KVartal_s%201995-2024.xlsx"),
+                       sheet_name="10", usecols='A:BA', skiprows=2, nrows=3)
+
+    columns = df.columns.tolist()
+
+    # Проходим по списку колонок и заменяем 'Unnamed' на ближайшую слева цифру
+    for i in range(len(columns)):
+        if "Unnamed" in str(columns[i]):
+            # Находим ближайшую слева цифру, используя регулярные выражения
+            # Предполагаем, что цифра всегда будет слева от 'Unnamed'
+            left_number = re.search(r'\d+', str(columns[i - 1])).group()
+            columns[i] = int(str(left_number)[:4])
+        columns[i] = int(str(columns[i])[:4])
+
+    # Обновляем имена колонок в DataFrame
+    df.columns = columns
+    new_df = pd.DataFrame(columns=["date", "gdp"])
+    i = 0
+    years = list(range(2011, 2025))
+    dates = []
+    values = df.T[1].tolist()
+    quaters = ['I квартал', 'II квартал', 'III квартал', 'IV квартал']
+    i = 0
+
+    for year in years:
+        for quater in quaters:
+            if i < len(values):
+                new_df = pd.concat([new_df, pd.DataFrame(
+                    {"date": [pd.to_datetime(to_date(year, quater))],
+                     "gdp": [values[i]]}
+                )], ignore_index=True)
+                i += 1
+    new_df.set_index('date', inplace=True)
+
+    # Заполняем пропущенные месяцы данными
+    df_resampled = new_df.resample('MS').ffill()
+    return df_resampled.reset_index()
+
+
 def get_gdp_dataframe():
     download_gdp()
     df = pd.read_excel(os.path.join(os.path.join("files", "gdp"), f"VVP_KVartal_s%201995-2024.xlsx"),
@@ -74,5 +115,5 @@ def get_gdp_dataframe():
 
 # download_gdp()
 if __name__ == "__main__":
-    df = get_gdp_dataframe()
+    df = get_gdp_dataframe_wout_seasons()
     print(transform_df_to_format(df))
