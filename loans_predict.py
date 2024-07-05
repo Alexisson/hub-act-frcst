c229f99deda074f6790c22898a83b1dff3b1a9ad
Pavel Zakharov <zotic1001@gmail.com>
2024-07-05 10:37:41 +0300

    Refactor

--- loans_predict.py
+++ loans_predict.py
@@ -19,30 +19,30 @@ class LoansPredict(Predictor):
     def __init__(self, datasource: PredictEnum):
         self.plot_title = ""
         if datasource == PredictEnum.MSP_NEW_LOANS:
-            self.datasource = get_new_loans_msp_df
+            self.__datasource = get_new_loans_msp_df
             self.plot_title = "Объем выданных кредитов МСП"
         elif datasource == PredictEnum.MSP_LOANS_VOLUME:
-            self.datasource = get_loans_volume_msp_df
+            self.__datasource = get_loans_volume_msp_df
             self.plot_title = "Задолженность по кредитам МСП"
         elif datasource == PredictEnum.RESIDENT_NEW_LOANS:
-            self.datasource = get_new_loans_resident_data
+            self.__datasource = get_new_loans_resident_data
             self.plot_title = "Объем выданных кредитов резиденты"
         elif datasource == PredictEnum.RESIDENT_LOANS_VOLUME:
-            self.datasource = get_resident_loans_volume_df
+            self.__datasource = get_resident_loans_volume_df
             self.plot_title = "Задолженность по кредитам резиденты"
         super().__init__()
         self.regions = get_measures()
-        self.regions_df = {}
+        self.__regions_df = {}
 
     def get_region_predict(self, region: int, predict_params: PredictInput = None):
         if predict_params is None:
             predict_params = PredictInput()
-        new_loans_volume_reg_df = self.datasource(2019, 2024, spikes_remove=False, measure_id=region)
+        new_loans_volume_reg_df = self.__datasource(2019, 2024, spikes_remove=False, measure_id=region)
         # определяем дату, до которой возьмем данные для обучения модели
         # Составляем прогноз
         self.dataset = new_loans_volume_reg_df
         predict_results = self.get_data_predict(predict_params)
-        self.regions_df[self.regions[region]] = predict_results
+        self.__regions_df[self.regions[region]] = predict_results
         return predict_results
 
     def get_regions_predict(self, regions: list[int] = None, predict_params=None):
@@ -54,9 +54,9 @@ class LoansPredict(Predictor):
         for key, value in self.regions.items():
             # Берем данные только по федеральным округам
             if key in regions:
-                if value not in self.regions_df:
-                    self.regions_df[value] = self.get_region_predict(key, predict_params)
-                predict, mp = self.regions_df[value].predict, self.regions_df[value].mape
+                if value not in self.__regions_df:
+                    self.__regions_df[value] = self.get_region_predict(key, predict_params)
+                predict, mp = self.__regions_df[value].predict, self.__regions_df[value].mape
                 results.append((value, predict))
         return results
 
@@ -66,11 +66,14 @@ class LoansPredict(Predictor):
         if predict_params is None:
             predict_params = PredictInput()
         res = self.get_regions_predict(regions, predict_params)
+        result = []
         for elem in res:
             print(elem[0], "\n")
+            result.append(elem[0])
             print("Спрогнозированные значения:")
             print(elem[1])
-            print("MAPE = ", self.regions_df[elem[0]].mape)
+            print("MAPE = ", self.__regions_df[elem[0]].mape)
+        return result
 
     def show_regions_plot(self, regions: list[int] = None, predict_params=None):
         if regions is None:
@@ -80,14 +83,13 @@ class LoansPredict(Predictor):
         for key, value in self.regions.items():
             # Берем данные только по федеральным округам
             if key in regions:
-                if value not in self.regions_df:
-                    self.regions_df[value] = self.get_region_predict(key, predict_params)
+                if value not in self.__regions_df:
+                    self.__regions_df[value] = self.get_region_predict(key, predict_params)
                 self.show_plot(
-                    PredictInput(self.regions_df[value].start_date, self.regions_df[value].last_predict_date),
+                    PredictInput(self.__regions_df[value].start_date, self.__regions_df[value].last_predict_date),
                     self.plot_title, value)
 
 
 if __name__ == "__main__":
-    predictor = LoansPredict(PredictEnum.RESIDENT_LOANS_VOLUME)
-    predictor.show_regions_plot()
-    predictor.show_regions_predict()
+    predictor = LoansPredict(PredictEnum.MSP_LOANS_VOLUME)
+    predictor.show_regions_plot([23, 42, 55, 64, 72, 87, 95, 106])
